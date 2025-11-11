791d0f3217612801c94f4fbe89f59402519f956a
Rayssa Santos <andy.rayssa37@gmail.com>
2025-11-11 16:58:47 -0300

    Add files via upload

---
+++ Vetores/Exercicio2.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner (System.in);
+		int i=0, tabuada;
+		
+		int a[] = new int[5];
+		
+			for(i = 0; i < 5; i++) {
+				System.out.println("Insira o "+(i+1)+" número do vetor A: ");
+				a[i] = ler.nextInt();
+			}
+			
+			System.out.println(" ");
+			System.out.println("Tabuada dos números inteiros do vetor A: ");
+			
+			for(i = 0; i <= 10; i++) {
+				tabuada = a[0] * i;
+				
+				System.out.println (a[0] + " x " + i + " = " + tabuada);
+			}
+			
+			System.out.println(" ");
+			
+			for(i = 0; i <= 10; i++) {
+				tabuada = a[1] * i;
+				
+				System.out.println (a[1] + " x " + i + " = " + tabuada);
+			}
+			
+			System.out.println(" ");
+			
+			for(i = 0; i <= 10; i++) {
+				tabuada = a[2] * i;
+				
+				System.out.println (a[2] + " x " + i + " = " + tabuada);
+			}
+			
+			System.out.println(" ");
+			
+			for(i = 0; i <= 10; i++) {
+				tabuada = a[3] * i;
+				
+				System.out.println (a[3] + " x " + i + " = " + tabuada);
+			}
+			
+			System.out.println(" ");
+			
+			for(i = 0; i <= 10; i++) {
+				tabuada = a[4] * i;
+				
+				System.out.println (a[4] + " x " + i + " = " + tabuada);
+			}
+			
+			System.out.println(" ");
+			
+			for(i = 0; i <= 10; i++) {
+				tabuada = a[5] * i;
+				
+				System.out.println (a[5] + " x " + i + " = " + tabuada);
+			}
+		
+			ler.close();
+		}
+	}
+	
