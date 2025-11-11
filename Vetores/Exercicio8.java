8170814930b3f87aaf1f622ebcaabb0906e5e58c
Rayssa Santos <andy.rayssa37@gmail.com>
2025-11-11 20:53:30 -0300

    Add files via upload

---
+++ Vetores/Exercicio8.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Exercicio8 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		int i=0, j=0, soma;
+
+		int a[] = new int[10];
+		int b[] = new int[10];
+		
+		for ( i = 0; i < 10; i++ ) {
+			
+			System.out.println("Insira o "+(i + 1)+" número do vetor A: ");
+			a[i] = ler.nextInt();
+			
+		}
+		
+		for ( i = 0; i < 10; i++ )  {
+			
+			soma = 0; 
+			
+			for ( j = i; j < 10; j++) {
+				
+				 soma = soma + a[j];
+			}
+			
+			b[i] = soma;
+		}
+		
+		for ( i = 0; i < 10; i++ ) {
+			
+			System.out.println("b[" + i + "] = " + b[i] + " (soma de a[" + i + "] até a[9])");
+			
+		}
+		
+		ler.close();
+	}
+
+}
